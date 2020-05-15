e05a3ff0d8e66fd00968c90eda3679d798a4b38d
James Coleman <jcolem22@msudenver.edu>
2020-05-15 10:53:24 -0600

    I created a migration and migrated it.

---
+++ game_data/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class GameData(models.Model):
+	season_end_year = models.PositiveIntegerField()
+	date = models.DateField()
+	win = models.BooleanField()
+	home = models.BooleanField()
+	oppt = models.CharField(max_length=3)
+	points_for = models.PositiveIntegerField()
+	points_against = models.PositiveIntegerField()
+
+
